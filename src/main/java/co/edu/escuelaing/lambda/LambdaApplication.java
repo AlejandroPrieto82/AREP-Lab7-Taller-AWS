921022e2faddec02920fe387837d661c4c06d48c
AlejandroPrieto82 <samuel.prieto-r@mail.escuelaing.edu.co>
2025-10-15 09:53:48 -0500

    Carpetas

---
+++ src/main/java/co/edu/escuelaing/lambda/LambdaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.lambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambdaApplication.class, args);
+	}
+
+}
